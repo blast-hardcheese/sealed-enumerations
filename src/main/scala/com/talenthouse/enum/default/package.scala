97696e15581986ad38aab9d64527bee9ec4b7ba2
Devon Stewart <blast@hardchee.se>
2016-04-27 16:44:35 -0700

    Adding default traits that mix in common features

---
+++ src/main/scala/com/talenthouse/enum/default/package.scala
@@ -0,0 +1,9 @@
+package com.talenthouse.enum
+
+package default {
+  trait SealedEnumeration
+    extends SealedOrderedEnumeration
+       with SealedBindableEnumeration
+       with SealedJsonEnumeration
+       with BaseSealedEnumeration
+}
